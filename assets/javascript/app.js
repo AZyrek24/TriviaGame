83e57373bfb516a4028236a41e19bdf921c48841
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-05-28 10:58:44 -0700

    fixed countdown timer in app.js, cleaned up html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,50 @@
-var correct = 0;
-var incorrect = 0;
-var timedOut = 0;
-var timer = 5;
-$(document).ready(function() {
+
+$(document).ready(function () {
   
   //Objects, Variables, and Arrays
   //=========================================================================================
+  var correct = 0;
+  var incorrect = 0;
+  var timedOut = 0;
+  var timerId;
   var game = {
     questions: {
       q1: ["What is the 'A.C.' stand for in A.C. Slater's name?",],
       a1: ["Albert Clifford", "Almond Cashew", "Aaron Charles", "Aybee Cee"]
     }
   }
-  console.log(game.questions[0]);
+  var timeLeft = 30;
+
   //Functions
   //=========================================================================================
-  
-
-  //Clicking the START button displays the timer and question
-  $("#start-button").on("click", function() {
-    $("#timer").html("<h2>Time: " + timer + "</h2>");
-    setInterval(countdownTimer, 1000);
-    countdownTimer();
-    $("#start-button").html("<h2>" + game.questions.q1[0] + "</h2><hr>");
-    for (var i = 0; i < game.questions.a1.length; i++) {
-    $("#answers").append("<h3>" + game.questions.a1[i] + "</h3>");
+  function countdown() {
+    if (timeLeft == 0) {
+      clearTimeout(timerId);
+    } else {
+      $("#timer").html("<h1>Time: " + timeLeft + "</h1>");
+      timeLeft--;
     }
-    // guessAnswer();
+  }
+
+
+  //Clicking the START button displays the timer(running), question, and answer choices
+  $("#start-button").on("click", function () {
+    $("#start-button").html("<h1>" + game.questions.q1[0] + "</h1><hr>");
+    $("#timer").html("<h1>Time: " + timeLeft + "</h1>");
+
+    timerId = setInterval(countdown, 1000);
+    
+    for (var i = 0; i < game.questions.a1.length; i++) {
+      $("#answers").append("<h3>" + game.questions.a1[i] + "</h3>");
+    }
+
   })
-  
-  
 })
+
+
+
+
 
 //Main Process
 //=========================================================================================
-var countdownTimer = function() {
-  timer--;
-  $("#timer").html("<h2>Time: " + timer + "</h2>");
-  if (timer === 0) {
-    timedOut++;
-    $("#start-button").text("Timed Out!");
-    clearInterval(countdownTimer);
-  }
-}
+
